b49bee26b215443a1f37476156d2f3050506f99f
Oksana Kyryliuk <coksanna@gmail.com>
2022-12-30 14:17:03 +0200

    clear input and add error message

--- src/js/search-movies.js
+++ src/js/search-movies.js
@@ -1,5 +1,6 @@
 import MoviesApi from './movie-service';
 import { createMarkup } from './createMarkup';
+import { clearMarkup } from './createMarkup';
 
 const formRef = document.querySelector('form');
 formRef.addEventListener('submit', onSearch);
@@ -7,12 +8,12 @@ formRef.addEventListener('submit', onSearch);
 const moviesApi = new MoviesApi();
 
 async function onSearch(e) {
-    e.preventDefault();      
+    e.preventDefault(); 
+    clearMarkup(); 
     moviesApi.query = e.currentTarget.elements.searchFilm.value.trim();
     moviesApi.resetPage();
-    await moviesApi.fetchMovieByQuery().then((data) => {
-        createMarkup(data);      
-    })
+    e.currentTarget.elements.searchFilm.value = '';
+    await moviesApi.fetchMovieByQuery().then((data) => createMarkup(data))
   }
 
 
