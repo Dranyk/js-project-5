f7c4a53c60c2ed5c461f3073dd16650b0596078f
Daniil <danielpanasenko777@gmail.com>
2023-01-03 21:43:40 +0200

    watched

--- src/js/modal-window.js
+++ src/js/modal-window.js
@@ -34,7 +34,7 @@ function onClickOutsideModal(e) {
   if (!modalInside) {
     closeModal();
   }
-  }
+}
 
 function clearModalInfo() {
   refs.renderModalCard.innerHTML = '';
@@ -63,3 +63,83 @@ async function createMarkupModal(e) {
 
 refs.openModalCard.addEventListener('click', createMarkupModal);
 refs.closeModalBtn.addEventListener('click', closeModal);
+
+async function createMarkupModal(e) {
+  const parentMovieCard = e.target.closest('.film-card__item');
+  const idMovieCard = parentMovieCard.dataset.id;
+
+  modalMoviesApi.id = idMovieCard;
+
+  await modalMoviesApi.fetchDetails().then(details => {
+    console.log(details.data);
+    clearModalInfo();
+    renderMarkupModal(details.data);
+    showModal();
+    const btWatched = document.querySelector('#title');
+    btWatched.addEventListener('click', handleClick);
+    window.addEventListener('keydown', onEscKeyPress);
+    document.addEventListener('click', onClickOutsideModal);
+  });
+}
+
+refs.openModalCard.addEventListener('click', createMarkupModal);
+refs.closeModalBtn.addEventListener('click', closeModal);
+const localStorageApi = {
+  //Проверяет хранилище по ключу. Возвращает: Пустой массив - если не находит, и Данные - если находит
+  getMovies(key) {
+    const keyStorage = this.load(key);
+
+    if (Array.isArray(keyStorage)) return keyStorage;
+
+    this.save(key, []);
+    return [];
+  },
+
+  //Добавляет фильм : Пушит переданный 'value' в LocalStorage с ключем 'key'
+  addMovie(key, value) {
+    const dataFromLocalStorage = this.load(key);
+    this.save(key, [value, ...dataFromLocalStorage]);
+  },
+
+  removeMovie(key, value) {
+    const dataFromLocalStorage = this.load(key);
+
+    const valueIndex = dataFromLocalStorage.indexOf(value);
+
+    if (0 <= valueIndex) {
+      dataFromLocalStorage.splice(valueIndex, 1);
+
+      this.save(key, dataFromLocalStorage);
+    }
+  },
+
+  // Принимает ключ `key` по которому будет произведена выборка.
+  load(key) {
+    try {
+      const serializedState = localStorage.getItem(key);
+
+      return serializedState === null ? undefined : JSON.parse(serializedState);
+    } catch (err) {
+      console.error('Get state error: ', err);
+    }
+  },
+
+  // Принимает ключ `key` и значение `value`.
+  save(key, value) {
+    try {
+      const serializedState = JSON.stringify(value);
+      localStorage.setItem(key, serializedState);
+    } catch (err) {
+      console.error('Set state error: ', err);
+    }
+  },
+};
+function handleClick(e) {
+  const storageKey = e.target.value;
+
+  const action = e.target.checked ? 'add' : 'remove';
+
+  localStorageApi.getMovies(storageKey);
+  localStorageApi.addMovie(storageKey);
+  localStorageApi.removeMovie(storageKey);
+}
--- src/js/watched.js
+++ src/js/watched.js
@@ -1,9 +1,51 @@
-import MoviesApi from './movie-service';
-import { modalWindowMovieMarkup } from './template-modal-movie';
+const localStorageApi = {
+  //Проверяет хранилище по ключу. Возвращает: Пустой массив - если не находит, и Данные - если находит
+  getMovies(key) {
+    const keyStorage = this.load(key);
 
-const btWatched = document.querySelector('.modal-movie__btn-watched');
+    if (Array.isArray(keyStorage)) return keyStorage;
 
-function handleClick() {
-  console.log('hello');
-}
-btWatched.addEventListener('click', handleClick);
+    this.save(key, []);
+    return [];
+  },
+
+  //Добавляет фильм : Пушит переданный 'value' в LocalStorage с ключем 'key'
+  addMovie(key, value) {
+    const dataFromLocalStorage = this.load(key);
+    this.save(key, [value, ...dataFromLocalStorage]);
+  },
+
+  removeMovie(key, value) {
+    const dataFromLocalStorage = this.load(key);
+
+    const valueIndex = dataFromLocalStorage.indexOf(value);
+
+    if (0 <= valueIndex) {
+      dataFromLocalStorage.splice(valueIndex, 1);
+
+      this.save(key, dataFromLocalStorage);
+    }
+  },
+
+  // Принимает ключ `key` по которому будет произведена выборка.
+  load(key) {
+    try {
+      const serializedState = localStorage.getItem(key);
+
+      return serializedState === null ? undefined : JSON.parse(serializedState);
+    } catch (err) {
+      console.error('Get state error: ', err);
+    }
+  },
+
+  // Принимает ключ `key` и значение `value`.
+  save(key, value) {
+    try {
+      const serializedState = JSON.stringify(value);
+      localStorage.setItem(key, serializedState);
+    } catch (err) {
+      console.error('Set state error: ', err);
+    }
+  },
+};
+export default localStorageApi;
