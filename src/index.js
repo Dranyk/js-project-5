0aa55fa228c9f8d2fb157931bc6a67677b5e7112
Dranyk <dvdranyk@gmail.com>
2023-01-04 17:26:02 +0200

    .

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import './js/movie-service';
+import './js/popular_film_key';
+import './js/popular_film';
+import './js/search-movies';
 import './js/createMarkup';
 import './js/template-modal-movie';
 import './js/modal-window';
