3f0d1e20b46376d04b111ce4a978a834f4234070
Dranyk <dvdranyk@gmail.com>
2023-01-07 09:12:01 +0200

    Update btnwatched.js

--- src/js/btnwatched.js
+++ src/js/btnwatched.js
@@ -1,5 +1,5 @@
 import { createWatched } from './createWatched';
-import { addWatched } from './watched';
+// import { addWatched } from './watched';
 
 const refs = {
   showWatchedBtn: document.querySelector('#showWatchedBtn'),
